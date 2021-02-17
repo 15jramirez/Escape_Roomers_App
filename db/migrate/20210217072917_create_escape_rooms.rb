7aa4042f43c57d06c3ca4ba623403fa906868d12
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-16 23:31:48 -0800

    removed test framework

--- db/migrate/20210217072917_create_escape_rooms.rb
+++ db/migrate/20210217072917_create_escape_rooms.rb
@@ -2,11 +2,8 @@ class CreateEscapeRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :escape_rooms do |t|
       t.string :title
-      t.text :description
       t.datetime :hours
-      t.string :location
-      t.string :diffculty
-
+      t.string :difficulty
       t.timestamps
     end
   end
