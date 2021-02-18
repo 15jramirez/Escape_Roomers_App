331ddc0985524418aa4db419626ab4621a919d22
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-17 21:18:55 -0800

    included helper in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_one :profile 
     has_secure_password
+    validates :username, presence:true, uniqueness:true 
+    validates :password, confirmation: true, unless: -> { password.blank? }
+
 end
